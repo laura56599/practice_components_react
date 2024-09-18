15dd9f4cb380afade705e58c844b88da3e529645
laura.velez <lauravelez1991@hotmail.com>
2024-09-17 20:08:15 -0500

    my first componente

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+import { MyFirstComponent } from './components/MyFirstComponent'
+
+function App() {
+ 
+  return (
+    <>
+      <MyFirstComponent />
+      <h1>Fundamentos de React</h1>
+      <ul>
+        <li>Componentes</li>
+        <li>Eventos</li>
+        <li>Estados Hooks</li>
+        <li>Props</li>
+        <li></li>
+        <li></li>
+      </ul>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/MyFirstComponent.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const MyFirstComponent = () => { 
+    return (
+        <>
+            <div>My First Component</div>
+            <h1>Hola, estoy aprendiendo a crear componentes en React</h1>
+        </>
+    )
+}
